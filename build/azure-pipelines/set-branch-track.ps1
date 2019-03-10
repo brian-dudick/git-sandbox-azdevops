dce1c9bd3066bd6c9208ea681e4b77ba2e208b40
Brian Dudick <bdudick@co-construct.com>
2019-03-10 19:04:57 -0400

    move set branch track to ps1

---
+++ build/azure-pipelines/set-branch-track.ps1
@@ -0,0 +1,22 @@
+
+Param(
+    [string]$branchSource
+    )
+    #$branchSource = ("$(Build.SourceBranchName)").ToString().ToUpper()
+     
+   #
+   if ($branchSource.ToUpper() -match 'ARA-') {$branchTrack = 'ARA'}
+   elseif ($branchSource.ToUpper() -match 'ARA2-') {$branchTrack = 'ARA2'}
+   elseif ($branchSource.ToUpper() -match 'CORE-') {$branchTrack = 'Core'}
+   elseif ($branchSource.ToUpper() -match 'CORE2-') {$branchTrack = 'Core2'}
+   elseif ($branchSource.ToUpper() -match 'MOBILE-') {$branchTrack = 'Mobile'}
+   elseif ($branchSource.ToUpper() -match 'QUALITY-') {$branchTrack = 'Quality'}
+   elseif ($branchSource.ToUpper() -match 'AZURE-DEVOPS-') {$branchTrack = 'Azure-DevOps'}
+   elseif ($branchSource.ToUpper() -match 'TEST-') {$branchTrack = 'Test'}
+   elseif ($branchSource.ToUpper() -match 'FEATURE-') {$branchTrack = 'Feature'}
+   elseif ($branchSource.ToUpper() -match 'RELEASE-') {$branchTrack = 'Release'}
+   elseif ($branchSource.ToUpper() -match 'ARCHITECTURE-'){$branchTrack = 'Architecture'}
+   elseif ($branchSource.ToUpper() -match 'DEVELOPMENT') {$branchTrack = 'Development'}
+
+   Write-Host "Assigning branchSource: $branchSource to branchTrack: $branchTrack " 
+   Write-Host "##vso[task.setvariable variable=COCON_BRANCH_TRACK;]$branchTrack"
