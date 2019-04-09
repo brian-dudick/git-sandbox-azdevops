5ef55ee9ed1511143dd4b2773bb4cfe973c4f941
brian-dudick <46999621+brian-dudick@users.noreply.github.com>
2019-04-09 09:43:14 -0400

    Development (#59)
    * debug build args
    
    * check branch type for build args
    
    * trim down debug statements
    
    * rearrange build argument evaluation
    
    * move msbuild param build-up to template
    
    * update 1153
    
    * update precompile options
    
    * update evaluation
    
    * Test Development
    
    * Check branch
    
    * Force CI
    
    * Feature 888 (#56)
    
    * add get-environment-variables
    
    * fix-up template name
    
    * fix-up path
    
    * test failed condition
    
    * only run on master
    
    * wild card feature test
    
    * contains 888
    
    * fix-up contains
    
    * test or condition
    
    * fix-up conditions
    
    * fix-up
    
    * move conditions to set msbuild params
    
    * test and
    
    * fix-up or
    
    * add release
    
    * force false condition
    
    * test conditions
    
    * fix-up or condition
    
    * test conditions
    
    * test branches
    
    * update to test for feature branch
    
    * get-envrionment
    
    * check branch tracj
    
    * get branch track
    
    * test branch track
    
    * check source
    
    * test
    
    * clean-up
    
    * clena-up branch track
    
    * fiddle
    
    * fiddle 2
    
    * test env
    
    * reset path
    
    * move to root
    
    * test
    
    * run me
    
    * test branch track
    
    * again
    
    * try again
    
    * one more time
    
    * fix parens
    
    * add comma
    
    * clean-up get branch
    
    * update department id
    
    * check branc track
    
    * update
    
    * force set
    
    * force set
    
    * update get and set
    
    * try set-item
    
    * change var
    
    * try last name
    
    * add setenvironment
    
    * again
    
    * add new
    
    * remove duplicate
    
    * one more time
    
    * alternatives
    
    * alt 1
    
    * alt 2
    
    * alt 3
    
    * alt 3 rev
    
    * secret sauce
    
    * force
    
    * use env as param
    
    * set lastname sullivan
    
    * force
    
    * force
    
    * try to throw error
    
    * only thing that seems to work
    
    * force
    
    * for set var
    
    * get the branch track
    
    * update saue
    
    * try one more time
    
    * gci
    
    * update set branch track
    
    * test branch track assignment
    
    * update set branch track with -match
    
    * clean-up set and get branch track
    
    * move set branch track to ps1
    
    * update default branch track
    
    * set default branchtrack
    
    * inlude bahubali and aztec
    
    * test create release notes
    
    * remove create release notes
    
    * remove github release (#57)
    
    * Development wip (#58)
    
    * remove github release
    
    * update to feature file
    
    * add timestamp
    
    * add updates to reflect devlopment wip
    
    * update pipeline

---
+++ build/azure-pipelines/create-release-notes.ps1
@@ -0,0 +1,20 @@
+﻿
+Param(
+    [string]$buildSourceBranchName,
+    [string]$buildSourceVersionMessage,
+    [string]$buildSourceVersion,
+    [string]$buildDefinitionName,
+    [string]$buildNumber
+    )
+
+    $releaseNotesMD  = [System.Text.StringBuilder]::new()
+    [void]$releaseNotesMD.Append("- Build Branch: $buildSourceBranchName ")
+    [void]$releaseNotesMD.Append("- Build Commit Message: $buildSourceVersionMessage ")
+    [void]$releaseNotesMD.Append("- Source Version: $buildSourceVersion ")
+    [void]$releaseNotesMD.Append("- Build Defition Name: $buildDefinitionName ")
+    [void]$releaseNotesMD.Append("- Build Number: $buildNumber ")
+
+    [void]$releaseNotesMD.AppendLine()
+    $releaseNotes = $releaseNotesMD.ToString()
+
+   Write-Host "##vso[task.setvariable variable=COCON_RELEASE_NOTES;]$releaseNotes)"
